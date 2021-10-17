8f28c4fbf24ef93e31e2c3d6b33c06d04893189f
kj7kunal <kjkunal7996@gmail.com>
2021-10-17 17:04:36 +0530

    changed about link color

--- src/components/section-about/index.jsx
+++ src/components/section-about/index.jsx
@@ -7,7 +7,7 @@ const SectionAbout = ({ about }) => {
     <Section title="About Me" link="/blog/more-about">
       <div className="mb-6">
         <p>
-          {about} <a class='hover:text-blue-500 hover:underline' href={`/blog/more-about`}>Learn More</a>
+          {about} <a class='text-gray-400 hover:text-blue-500 hover:underline' href={`/blog/more-about`}>Learn More</a>
         </p>
       </div>
     </Section>
