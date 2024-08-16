623db8fd438440649ee6056b7ac63cac842b01ad
kj7kunal <kjkunal7996@gmail.com>
2024-08-16 19:24:42 +0900

    update visional blog link

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,7 @@ module.exports = {
       {
         name: 'Visional Inc. (formerly BizReach Inc.)',
         description: 'Machine Learning Engineer, October 2019 - Present',
-        link: '/blog/2021-visional',
+        link: '/blog/2024-visional',
       },
       {
         name: 'Nanyang Technological University, Singapore',
